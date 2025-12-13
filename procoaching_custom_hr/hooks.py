acbe1c1ce6258434cfb8e9d05856b21e4f23a911
Guna Sekhar <manchemsekhar3@gmail.com>
2025-12-13 16:49:51 +0530

    Comment out app_include_js for roster error guard

--- procoaching_custom_hr/hooks.py
+++ procoaching_custom_hr/hooks.py
@@ -6,9 +6,8 @@ app_email = "info@pro-coaching.co.uk"
 app_license = "MIT"
 
 # Include JS guard to prevent roster onError crash
-app_include_js = [
-    "/assets/procoaching_custom_hr/js/roster_error_guard.js"
-]
+# app_include_js = [
+ #   "/assets/procoaching_custom_hr/js/roster_error_guard.js"]
 
 override_whitelisted_methods = {
     "hrms.api.roster.get_events": "procoaching_custom_hr.overrides.roster_override.get_events"
